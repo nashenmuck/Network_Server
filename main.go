3d72c26c4e82ba8c856ca43f4f6dc71701800f2e
David Muckle <dvdmuckle@dvdmuckle.xyz>
2018-03-26 19:49:35 -0400

    Add dummy handler
    Shut up k8s, it works fine!

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/handlers"
+	"log"
+	"net/http"
+	"os"
+)
+
+func dummyrequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "We're up!")
+}
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Printf("Serving on port %s\n", os.Getenv("PORT"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		dummyrequest(w, r)
+	})
+	log.Fatal(http.ListenAndServe(":"+(os.Getenv("PORT")),
+		handlers.LoggingHandler(os.Stdout, http.DefaultServeMux)))
 }
