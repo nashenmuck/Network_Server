e2bbe79cc502a42ee659ebfd68794bb2d0e99670
David Muckle <dvdmuckle@dvdmuckle.xyz>
2018-04-12 22:43:09 -0400

    Initial posts and followers tables

---
+++ sql/2_followers.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE followers {
+followeeId int NOT NULL,
+followerId int NOT NULL,
+followedwhen timestamp NOT NULL
+};
---
+++ sql/2_posts.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE posts {
+user varchar(32) NOT NULL,
+body text NOT NULL,
+group varchat(32) NOT NULL
+};
