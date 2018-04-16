2ca769c68a1617a1b5aafab5d5106979883b4e61
David Muckle <dvdmuckle@dvdmuckle.xyz>
2018-04-16 16:40:56 -0400

    Initial full table defs

--- sql/2_initialize.up.sql
+++ sql/2_initialize.up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS followers (
 	followeeId int NOT NULL,
-	followerId int NOT NULL,
-	followedwhen timestamp NOT NULL
+	followerId int NOT NULL, followedwhen timestamp NOT NULL
 );
 CREATE TABLE IF NOT EXISTS groups (
 	groupname varchar(32) NOT NULL,
@@ -10,12 +9,28 @@ CREATE TABLE IF NOT EXISTS groups (
 CREATE TABLE IF NOT EXISTS posts (
 	username varchar(32) NOT NULL,
 	body text NOT NULL,
-	groupname varchar(32) NOT NULL
+	groupname varchar(32) NOT NULL,
+	server varchar NOT NULL
 );
 CREATE TABLE IF NOT EXISTS users (
 	username varchar(32) PRIMARY KEY NOT NULL,
 	password bytea NOT NULL,
 	salt bytea NOT NULL,
 	isadmin boolean DEFAULT FALSE,
-	parentAdmin int
+	parentAdmin varchar(32)
 );
+CREATE TABLE IF NOT EXISTS servers (
+	server varchar NOT NULL,
+	blacklisted boolean DEFAULT FALSE,
+	whitelisted boolean DEFAULT TRUE
+);
+CREATE TABLE IF NOT EXISTS authtokens (
+	username varchar(32) NOT NULL,
+	token bytea NOT NULL
+);
+CREATE TABLE IF NOT EXISTS regtokens (
+	issuer varchar(32) NOT NULL,
+	token bytea NOT NULL
+);
+
+
