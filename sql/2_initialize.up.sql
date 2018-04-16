a54965f5d28f8cf5384cc021bd6571d9e206b709
David Muckle <dvdmuckle@dvdmuckle.xyz>
2018-04-16 18:55:34 -0400

    Make atributes unique where sensibile

--- sql/2_initialize.up.sql
+++ sql/2_initialize.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS followers (
 	followeeId int NOT NULL,
-	followerId int NOT NULL, followedwhen timestamp NOT NULL
+	followerId int NOT NULL, 
+	followedwhen timestamp NOT NULL
 );
 CREATE TABLE IF NOT EXISTS groups (
-	groupname varchar(32) NOT NULL,
+	groupname varchar(32) NOT NULL UNIQUE,
 	username varchar(32) NOT NULL
 );
 CREATE TABLE IF NOT EXISTS posts (
@@ -13,24 +14,24 @@ CREATE TABLE IF NOT EXISTS posts (
 	server varchar NOT NULL
 );
 CREATE TABLE IF NOT EXISTS users (
-	username varchar(32) PRIMARY KEY NOT NULL,
+	username varchar(32) PRIMARY KEY NOT NULL UNIQUE,
 	password bytea NOT NULL,
-	salt bytea NOT NULL,
+	salt bytea NOT NULL UNIQUE,
 	isadmin boolean DEFAULT FALSE,
 	parentAdmin varchar(32)
 );
 CREATE TABLE IF NOT EXISTS servers (
-	server varchar NOT NULL,
+	server varchar NOT NULL UNIQUE,
 	blacklisted boolean DEFAULT FALSE,
 	whitelisted boolean DEFAULT TRUE
 );
 CREATE TABLE IF NOT EXISTS authtokens (
 	username varchar(32) NOT NULL,
-	token bytea NOT NULL
+	token bytea NOT NULL UNIQUE
 );
 CREATE TABLE IF NOT EXISTS regtokens (
 	issuer varchar(32) NOT NULL,
-	token bytea NOT NULL
+	token bytea NOT NULL UNIQUE
 );
 
 
