3c2480d70befb8dd6b9b6b07ff0aaefb2fb27533
David Muckle <dvdmuckle@dvdmuckle.xyz>
2018-04-17 14:09:51 -0400

    Reorder table defs, fix typos

--- sql/2_initialize.up.sql
+++ sql/2_initialize.up.sql
@@ -1,27 +1,7 @@
-CREATE TABLE IF NOT EXISTS followers (
-	followeeId varchar(32) NOT NULL REFERENCES users(username),
-	followee_server varchar NOT NULL REFERENCES servers(server),
-	followerId varchar(32) NOT NULL REFERENCES users(username), 
-	follower_server varchar NOT NULL REFERENCES servers(servers),
-	followedwhen timestamp NOT NULL
-);
-CREATE TABLE IF NOT EXISTS group_followers (
-	group_id int NOT NULL REFERENCES groups(group_id),
-	follower varchar(32) NOT NULL REFERENCES user(username),
-	follower_server varchar references servers(server),
-	UNIQUE (group_id, follower)
-);
-CREATE TABLE IF NOT EXISTS groups (
-	group_id int PRIMARY KEY,
-	owner varchar(32) NOT NULL REFERENCES users(username),
-	PRIMARY KEY (group_id, owner),
-);
-CREATE TABLE IF NOT EXISTS posts (
-	username varchar(32) NOT NULL REFERENCES users(username),
-	body text NOT NULL,
-	groupid int NOT NULL REFERENCES groups(group_id),
-	is_special_group boolean NOT NULL,
-	origin_server varchar NOT NULL REFERENCES servers(server),
+CREATE TABLE IF NOT EXISTS servers (
+	server varchar NOT NULL UNIQUE,
+	blacklisted boolean DEFAULT FALSE,
+	whitelisted boolean DEFAULT FALSE
 );
 CREATE TABLE IF NOT EXISTS users (
 	username varchar(32) NOT NULL UNIQUE PRIMARY KEY,
@@ -32,10 +12,30 @@ CREATE TABLE IF NOT EXISTS users (
 	canonical_user varchar(32) NOT NULL,
 	canonical_server varchar NOT NULL REFERENCES servers(server)
 );
-CREATE TABLE IF NOT EXISTS servers (
-	server varchar NOT NULL UNIQUE,
-	blacklisted boolean DEFAULT FALSE,
-	whitelisted boolean DEFAULT FALSE
+CREATE TABLE IF NOT EXISTS groups (
+	group_id int PRIMARY KEY,
+	owner varchar(32) NOT NULL REFERENCES users(username),
+	PRIMARY KEY (group_id, owner)
+);
+CREATE TABLE IF NOT EXISTS followers (
+	followeeId varchar(32) NOT NULL REFERENCES users(username),
+	followee_server varchar NOT NULL REFERENCES servers(server),
+	followerId varchar(32) NOT NULL REFERENCES users(username), 
+	follower_server varchar NOT NULL REFERENCES servers(server),
+	followedwhen timestamp NOT NULL
+);
+CREATE TABLE IF NOT EXISTS group_followers (
+	group_id int NOT NULL REFERENCES groups(group_id),
+	follower varchar(32) NOT NULL REFERENCES users(username),
+	follower_server varchar references servers(server),
+	UNIQUE (group_id, follower)
+);
+CREATE TABLE IF NOT EXISTS posts (
+	username varchar(32) NOT NULL REFERENCES users(username),
+	body text NOT NULL,
+	groupid int NOT NULL REFERENCES groups(group_id),
+	is_special_group boolean NOT NULL,
+	origin_server varchar NOT NULL REFERENCES servers(server)
 );
 CREATE TABLE IF NOT EXISTS authtokens (
 	username varchar(32) NOT NULL REFERENCES users(username), 
