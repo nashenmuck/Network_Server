97ef7222edb7d7a28446390b1dbe712833991255
David Muckle <dvdmuckle@dvdmuckle.xyz>
2018-04-18 17:53:25 -0400

    Initial SQL definitions (#2)
    * Initial user defs
    
    * Initial posts and followers tables
    
    * Create initial groups table, properly format deffs, tune migration
    
    * Create initial groups table, properly format deffs, tune migration
    
    * Create up and down migrations
    
    * Only create the tables if they don't exist
    
    * Put all table defs into one file
    
    * Initial full table defs
    
    * Make atributes unique where sensibile
    
    * Update 2_initialize.up.sql
    
    Add foreign keys
    Add server components of the username  where relevant
    Add a mechanism for forwarding
    
    * Update 2_initialize.up.sql
    
    * Reorder table defs, fix typos
    
    * Update 2_initialize.up.sql
    
    * Give group_id a UNIQUE constraint on the column
    
    * Add audit table
    
    * Create indexes

---
+++ sql/2_initialize.up.sql
@@ -0,0 +1,59 @@
+CREATE TABLE IF NOT EXISTS servers (
+	server varchar NOT NULL UNIQUE,
+	blacklisted boolean DEFAULT FALSE,
+	whitelisted boolean DEFAULT FALSE
+);
+CREATE TABLE IF NOT EXISTS users (
+	username varchar(32) NOT NULL UNIQUE PRIMARY KEY,
+	password bytea NOT NULL,
+	salt bytea NOT NULL UNIQUE,
+	isadmin boolean DEFAULT FALSE,
+	parentAdmin varchar(32),
+	canonical_user varchar(32) NOT NULL,
+	canonical_server varchar NOT NULL REFERENCES servers(server)
+);
+CREATE TABLE IF NOT EXISTS groups (
+	group_id int NOT NULL UNIQUE,
+	owner varchar(32) NOT NULL REFERENCES users(username),
+	group_name varchar(32) NOT NULL,
+	PRIMARY KEY (group_id, owner),
+	UNIQUE (owner,group_name)
+);
+CREATE TABLE IF NOT EXISTS followers (
+	followeeId varchar(32) NOT NULL REFERENCES users(username),
+	followee_server varchar NOT NULL REFERENCES servers(server),
+	followerId varchar(32) NOT NULL REFERENCES users(username), 
+	follower_server varchar NOT NULL REFERENCES servers(server),
+	followedwhen timestamp NOT NULL DEFAULT NOW()
+);
+CREATE TABLE IF NOT EXISTS group_followers (
+	group_id int NOT NULL REFERENCES groups(group_id),
+	follower varchar(32) NOT NULL REFERENCES users(username),
+	follower_server varchar references servers(server),
+	UNIQUE (group_id, follower)
+);
+CREATE TABLE IF NOT EXISTS posts (
+	username varchar(32) NOT NULL REFERENCES users(username),
+	body text NOT NULL,
+	groupid int NOT NULL REFERENCES groups(group_id),
+	is_special_group boolean NOT NULL,
+	origin_server varchar NOT NULL REFERENCES servers(server),
+	date timestamp NOT NULL DEFAULT NOW()
+);
+CREATE TABLE IF NOT EXISTS authtokens (
+	username varchar(32) NOT NULL REFERENCES users(username), 
+	token bytea NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS regtokens (
+	issuer varchar(32) NOT NULL REFERENCES users(username),
+	token bytea NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS audit (
+	data timestamp NOT NULL,
+	event_type varchar,
+	event_message varchar
+);
+CREATE INDEX user_index ON users (username);
+CREATE INDEX groups_index ON groups (group_id,owner);
+CREATE INDEX posts_user_index ON posts (username);
+CREATE INDEX posts_time_index ON posts (date);
