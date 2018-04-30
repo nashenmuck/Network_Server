c84588d2e8605326fba41f92eef8633e41898135
David Muckle <dvdmuckle@dvdmuckle.xyz>
2018-04-30 03:02:35 -0400

    Delete invite token on registration

--- auth/invite.go
+++ auth/invite.go
@@ -46,18 +46,47 @@ func RegUser(w http.ResponseWriter, r *http.Request, db *sql.DB, servername stri
 	var newUser netjson.Users
 	err := newUser.Decode(w, r)
 	if err != nil {
+		http.Error(w, "Internal server error", 500)
 		return
 	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
 	if err != nil {
+		http.Error(w, "Internal server error", 500)
 		log.Println(err)
+		return
 	}
-	stmt, err := db.Prepare("INSERT INTO users(username, password, isadmin, canonical_user, canonical_server) VALUES($1, $2, $3, $4, $5)")
+	insertStmt, err := db.Prepare("INSERT INTO users(username, password, isadmin, canonical_user, canonical_server) VALUES($1, $2, $3, $4, $5)")
+	if err != nil {
+		http.Error(w, "Internal server error", 500)
+		log.Println(err)
+		return
+	}
+	defer insertStmt.Close()
+	_, err = insertStmt.Exec(newUser.Username, hash, false, newUser.Username, servername)
+	if err != nil {
+		http.Error(w, "Internal server error", 500)
+		log.Println(err)
+		return
+	}
+	deleteStmt, err := db.Prepare("DELETE FROM regtokens WHERE token=$1")
+	if err != nil {
+		http.Error(w, "Internal server error", 500)
+		log.Println(err)
+		return
+	}
+	defer deleteStmt.Close()
+	stringTok, err := uuid.FromString(r.Header.Get("Reg-Token"))
 	if err != nil {
 		log.Println(err)
+		http.Error(w, "Internal server error", 500)
+		return
 	}
-	defer stmt.Close()
-	_, err = stmt.Exec(newUser.Username, hash, false, newUser.Username, servername)
+	_, err = deleteStmt.Exec(stringTok.Bytes())
+	if err != nil {
+		http.Error(w, "Internal server error", 500)
+		log.Println(err)
+		return
+	}
 	fmt.Fprintln(w, "Registered")
 	return
 }
